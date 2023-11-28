215359066607219c20b2876f7c9da96ceb894b17
chungesudeshna <chungesudeshna@gmail.com>
2023-11-28 15:08:36 +0530

    first

--- hi.java
+++ hi.java
@@ -1,11 +1,9 @@
 class Simple{  
+public static void main(String args[]){  
+System.out.println("Hello radhakrishna");  
 	
-    public static void main(String args[]){  
+}  
 	
-     System.out.println("Hello radhakrishna");  
-	
-    }  
-	
-} 
+  } 
 
 
